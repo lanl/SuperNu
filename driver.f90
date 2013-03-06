0b4da3364f4ecee62f6aecbc42af796157dd6b2d
D.R. van Rossum <daan@flash.uchicago.edu>
2013-03-05 22:23:55 -0600

    modified driver.f90 to comment out all new parts (which are not working yet)

--- driver.f90
+++ driver.f90
@@ -1,22 +1,22 @@
 PROGRAM driver
 
-  use mpimod
+!!use mpimod
   USE inputparmod
   USE timestepmod
   USE gasgridmod
   USE particlemod
 
-  use ionsmod, only:ion_read_data,ion_alloc_grndlev
-  use bfxsmod, only:bfxs_read_data
-  use ffxsmod, only:ffxs_read_data
-  use timingmod
+!!use ionsmod, only:ion_read_data,ion_alloc_grndlev
+!!use bfxsmod, only:bfxs_read_data
+!!use ffxsmod, only:ffxs_read_data
+!!use timingmod
 
   IMPLICIT NONE
 
   INTEGER(iknd) :: it, ipart
   REAL(rknd) :: time_begin, time_end
-  logical :: lmpi0 = .false. !master rank flag
-  real :: t0,t1  !timing
+!!logical :: lmpi0 = .false. !master rank flag
+!!real :: t0,t1  !timing
 ************************************************************************
 * Main routine
 *
@@ -25,9 +25,9 @@ PROGRAM driver
 ************************************************************************
 !
 !-- mpi initialization
-  call mpi_init(ierr) !MPI
-  call mpi_comm_rank(MPI_COMM_WORLD,impi,ierr) !MPI
-  call mpi_comm_size(MPI_COMM_WORLD,nmpi,ierr) !MPI
+!!call mpi_init(ierr) !MPI
+!!call mpi_comm_rank(MPI_COMM_WORLD,impi,ierr) !MPI
+!!call mpi_comm_size(MPI_COMM_WORLD,nmpi,ierr) !MPI
 !
 !--
 !-- SETUP SIMULATION:
@@ -35,15 +35,15 @@ PROGRAM driver
 !-- The setup is done by the master task only, and broadcasted to the
 !-- other tasks before packet propagation begins.
 !--
-  if(impi==impi0) then
-   lmpi0 = .true. !master rank flag
-   call time(t0)
+!!if(impi==impi0) then
+!! lmpi0 = .true. !master rank flag
+!! call time(t0)
 !-- startup message
-   call banner
+!! call banner
 !-- read runtime parameters
-   call read_inputpars
+!! call read_inputpars
 !-- parse runtime parameters
-   call parse_inputpars(nmpi)
+!! call parse_inputpars(nmpi)
 !
 !-- time step init
   CALL timestep_init(in_nt)
@@ -54,24 +54,24 @@ PROGRAM driver
 !-- SETUP GRIDS
 !-- setup gas grid (map gstruct to ggrid)
    CALL gasgrid_init(in_nr,in_ng)
-   call ggrid_setup(ncg,tim_ntim)
+!! call ggrid_setup(ncg,tim_ntim)
 !-- read initial temperature structure from file
    !call read_restart_file
 !
 !-- READ DATA
 !-- read ion and level data
-   call ion_read_data(gas_nelem)  !ion and level data
-   call ion_alloc_grndlev(ncg)   !ground state occupation numbers
+!! call ion_read_data(gas_nelem)  !ion and level data
+!! call ion_alloc_grndlev(ncg)   !ground state occupation numbers
 !-- read bbxs data
-   if(.not.in_nobbopac) call read_bbxs_data(gas_nelem)!bound-bound cross section data
+!! if(.not.in_nobbopac) call read_bbxs_data(gas_nelem)!bound-bound cross section data
 !-- read bfxs data
-   if(.not.in_nobfopac) call bfxs_read_data          !bound-free cross section data
+!! if(.not.in_nobfopac) call bfxs_read_data          !bound-free cross section data
 !-- read bfxs data
-   if(.not.in_noffopac) call ffxs_read_data          !free-free cross section data
+!! if(.not.in_noffopac) call ffxs_read_data          !free-free cross section data
 !
-   call time(t1)
-   t_setup = t1-t0
-  endif !impi
+!! call time(t1)
+!! t_setup = t1-t0
+!!endif !impi
   
   
   ! Setting velocity option
@@ -123,18 +123,18 @@ PROGRAM driver
 !--
 !-- FINISH UP:
 !=============
-  call mpi_barrier(MPI_COMM_WORLD,ierr) !MPI
+!!call mpi_barrier(MPI_COMM_WORLD,ierr) !MPI
 !-- Print timing output.
-  if(lmpi0) then
-   call time(t1)
-   t_all = dble(t1 - t0)
-   call print_timing                 !print timing results
-   write(6,*)
-   write(6,*) 'SuperNu finished'
-   if(in_grab_stdout)write(0,'(a,f8.2,"s")')'SuperNu finished',t_all!repeat to stderr
-  endif
+!!if(lmpi0) then
+!! call time(t1)
+!! t_all = dble(t1 - t0)
+!! call print_timing                 !print timing results
+!! write(6,*)
+!! write(6,*) 'SuperNu finished'
+!! if(in_grab_stdout)write(0,'(a,f8.2,"s")')'SuperNu finished',t_all!repeat to stderr
+!!endif
 !-- Clean up memory. (This help to locate memory leaks)
-  call dealloc_all
-  call mpi_finalize(ierr) !MPI
+!!call dealloc_all
+!!call mpi_finalize(ierr) !MPI
 
 END PROGRAM driver
