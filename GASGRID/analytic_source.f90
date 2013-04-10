deeb4e835ac46e2cf0c9311dbda298fa8830566b
Ryan Wollaeger <wollaeger@wisc.edu>
2013-04-10 15:16:48 -0500

    added manufactured source to analytic_source subroutine. must still be tested.

--- GASGRID/analytic_source.f90
+++ GASGRID/analytic_source.f90
@@ -4,6 +4,7 @@ subroutine analytic_source
   use physconstmod
   use timestepmod
   use particlemod
+  use inputparmod
   implicit none
 
   integer :: ir, ig
@@ -53,8 +54,89 @@ subroutine analytic_source
         enddo
      enddo
   elseif(gas_srctype=='manu') then
+     !
      !Manufactured Source (for gas_grptype='line')
-     stop 'analytic_source: manu not yet available'
+     if(gas_grptype.ne.'line') then
+        stop 'analytic_source: gas_grptype=line for gas_srctype=manu'
+     endif
+     !
+     real*8 :: uudd = 2.5d8  !velocity gauss width
+     real*8 :: tmpgauss(nr)  !manufactured gaussian temperature
+     real*8 :: eradmanu(ng,nr) !manufactured rad. en. density
+     
+     real*8 :: aa11=1.371d14
+     real*8 :: aa22=1.371d11
+     real*8 :: rrcenter, aa00, bspeced, xx3, xx4
+
+     if(gas_isvelocity) then
+        !calculating false gaussian temperature
+        do ir = 1, nr
+           !cell center
+           rrcenter=(gas_rarr(ir+1)+gas_rarr(ir))/2d0
+           !
+           tmpgauss(ir)=gas_templ0*exp(-0.5d0*(rrcenter/uudd)**2) &
+                *in_tfirst*pc_day*/tsp_texp
+           !
+           !Thin lines
+           do ig = 1, ng, 2
+              x3 = 1d0/gas_wl(ig+1)
+              x4 = 1d0/gas_wl(ig)
+              !calculating false thin group rad. energy density
+              eradmanu(ig,ir)=((aa11*(in_velout-rrcenter)+ &
+                   aa22*rrcenter)/in_velout)* &
+                   (in_tfirst*pc_day/tsp_texp)**4 &
+                   *(x4-x3)/(x2-x1)
+              !
+              !calculating manufactured source
+              xx3 = x3*pc_h*pc_c/(1d3*pc_ev*tmpgauss(ir))
+              xx4 = x4*pc_h*pc_c/(1d3*pc_ev*tmpgauss(ir))
+              aa00=in_tfirst*pc_day*in_velout
+              bspeced = 15d0*specint(xx3,xx4,3)/pc_pi**4
+              !
+              gas_exsource(ig,ir) = &
+                   (aa00*aa11/(pc_day*in_tfirst*in_velout)**5 - &
+                   5.0*eradmanu(ig,ir)/(tsp_texp*in_velout))*in_velout &
+                   +2.0*pc_c*eradmanu(ig,ir)/rrcenter + &
+                   pc_c*(aa22-aa11)*aa00/(tsp_texp*in_velout)**5 - &
+                   pc_c*bspeced*gas_sigmapg(ig,ir)*pc_acoef*tmpgauss**4 &
+                   gas_sigmapg(ig,ir)*rrcenter*(pc_c-4d0*rrcenter/3d0)* &
+                   eradmanu(ig,ir)/pc_c
+              !
+              write(*,*) 'thin: ',gas_exsource(ig,ir)
+              !
+           enddo
+           !
+           !Thick lines
+           do ig = 2, ng, 2
+              x3 = 1d0/gas_wl(ig+1)
+              x4 = 1d0/gas_wl(ig)
+              !calculating false thick group rad. energy density
+              eradmanu(ig,ir)=pc_acoef*tmpgauss**4*(x4-x3)/(x2-x1)
+              !
+              !calculating manufactured source
+              xx3 = x3*pc_h*pc_c/(1d3*pc_ev*tmpgauss(ir))
+              xx4 = x4*pc_h*pc_c/(1d3*pc_ev*tmpgauss(ir))
+              bspeced = 15d0*specint(xx3,xx4,3)/pc_pi**4
+              !
+              gas_exsource(ig,ir) = eradmanu(ig,ir)* &
+                   (4d0*((rrcenter*tsp_texp)**2*uudd/ &
+                   (uudd*tsp_texp)**3-in_velout/(uudd*tsp_exp) &
+                   +pc_c*(3d0*(uudd*tsp_exp)**2- &
+                   2.0*(rrcenter*tsp_texp)**2) &
+                   /(3d0*gas_sigmapg(ig,ir)*(uudd*tsp_texp)**4)) &
+                   -pc_c*bspeced*gas_sigampg(ig,ir)*(x2-x1)/(x4-x3) &
+                   +pc_c*gas_sigmap(ig,ir) &
+                   +4.0*gas_sigmapg(ig,ir)*rrcenter*(rrcenter &
+                   -pc_c*rrcenter/(gas_sigmapg(ig,ir)*tsp_texp*uudd**2)))
+              !
+              write(*,*) 'thick: ',gas_exsource(ig,ir)
+              !
+           enddo
+           !
+        enddo
+     else
+        stop 'analytic_source: no static manufactured source'
+     endif
   else
      stop 'analytic_source: gas_srctype invalid'
   endif
