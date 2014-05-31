d0a997b2e316be997123c5fce07c4200a7957b7e
D.R. van Rossum <daan@flash.uchicago.edu>
2014-05-31 15:58:12 -0500

    avoid non-standard e number representations in output.Lum files

--- write_output.f90
+++ write_output.f90
@@ -8,7 +8,10 @@ subroutine write_output
   implicit none
 
   integer :: ir, ig, j
+  integer :: reclen
   character(16), save :: pos='rewind', fstat='replace'
+!
+  reclen = gas_ng*12
 
   if(.not.in_isbdf2.or.(in_isbdf2.and.tsp_it>1)) then
 
@@ -25,14 +28,13 @@ subroutine write_output
   write(4,*) tsp_t
   close(4)
 
-  open(unit=4,file='output.LumNum',status=fstat,position='append',recl=gas_ng*16)
-  write(4,'(10000i16)') gas_lumnum
+  open(unit=4,file='output.LumNum',status=fstat,position='append',recl=reclen)
+  write(4,'(10000i12)') gas_lumnum
   close(4)
 
-  open(unit=4,file='output.Lum',status='unknown',position=pos)
-  do ig = 1, gas_ng
-     write(4,'(es16.8)',advance='no') gas_luminos(ig)
-  enddo
+  open(unit=4,file='output.Lum',status=fstat,position='append',recl=reclen)
+  where(gas_luminos<1d-99) gas_luminos = 1d-99  !prevent non-universal number representation, e.g. 1.1234-123
+  write(4,'(1p,10000e12.4)') gas_luminos
   close(4)
 
   open(unit=4,file='output.devLum',status='unknown',position=pos)
@@ -45,10 +47,8 @@ subroutine write_output
   enddo
   close(4)
 
-  open(unit=4,file='output.temp',status='unknown',position=pos)
-  do ir = 1, gas_nr
-    write(4,'(es16.8)',advance='no') gas_tempold(ir)
-  enddo
+  open(unit=4,file='output.temp',status=fstat,position='append',recl=reclen)
+  write(4,'(1p,10000e12.4)') gas_tempold
   close(4)
 
   open(unit=4,file='output.gas_fcoef',status='unknown',position=pos)
