3039bfd3219978005b72698a85ee7af31b8963cb
Ryan Wollaeger <wollaeger@wisc.edu>
2014-10-24 15:39:30 -0500

    added geometry selection to initial_particles.  should be similar in other particle generation routines.

--- initial_particles.f90
+++ initial_particles.f90
@@ -12,16 +12,18 @@ subroutine initial_particles
   !the first time step.
 !##################################################
 !
+  logical :: lhelp
   integer :: ig, i,j,k iig, ipart
   integer, dimension(gas_nx,gas_ny,gas_nz) :: ijkused
-  real*8 :: wl0, mu0, ep0, r0
+  real*8 :: wl0, mu0, ep0, x0, y0, z0
   real*8 :: denom2
-  real*8 :: r1,r3
-  real*8 :: x1,x2,x3,x4
+  real*8 :: r1
+  real*8 :: wl1,wl2,wl3,wl4
+  real*8 :: cmffact, azitrfm
 
 !-- helper quantities
-  x1=1d0/gas_wl(gas_ng+1)
-  x2=1d0/gas_wl(1)
+  wl1=1d0/gas_wl(gas_ng+1)
+  wl2=1d0/gas_wl(1)
 
 !-- instantiating initial particles
   i = 1
@@ -29,70 +31,131 @@ subroutine initial_particles
   k = 1
   ijkused = 0
   do ipart=1,prt_ninitnew
+
+!-- incrementing to next vacant cell
      do k=k,gas_nz
-     do j=j,gas_ny
-     do i=i,gas_nx
-        if(ijkused(i,j,k)<gas_nvolinit(i,j,k)) exit
+        do j=j,gas_ny
+           do i=i,gas_nx
+              lhelp = ijkused(i,j,k)<gas_nvolinit(i,j,k)
+              if(lhelp) exit
+           enddo
+           if(lhelp) exit
+        enddo
+        if(lhelp) exit
      enddo
-     enddo
-     enddo
-!--
+!-- increasing cell occupancy
      ijkused(i,j,k) = ijkused(i,j,k)+1
+!
+!-- setting i,j,k = zone of particle
+     prt_particles(ipart)%zsrc = i
+     prt_particles(ipart)%iy = j
+     prt_particles(ipart)%iz = k
+
+!-- setting particle index to not vacant
+     prt_isvacant(ipart) = .false.
+!
+!-- calculating particle time
+     prt_particles(ipart)%tsrc = tsp_t
+!
+!-- setting type
+     prt_particles(ipart)%rtsrc = 1
+
+!-- calculating wavelegth
      denom2 = 0d0
      r1=rand()
      prt_tlyrand = prt_tlyrand+1
      do ig = 1, gas_ng
-        x3 = 1d0/gas_wl(ig+1)
-        x4 = 1d0/gas_wl(ig)
+        wl3 = 1d0/gas_wl(ig+1)
+        wl4 = 1d0/gas_wl(ig)
         iig = ig
-        if(r1>=denom2.and.r1<denom2 + (x4-x3)/(x2-x1)) exit
-        denom2 = denom2 + (x4-x3)/(x2-x1)
+        if(r1>=denom2.and.r1<denom2 + (wl4-wl3)/(wl2-wl1)) exit
+        denom2 = denom2 + (wl4-wl3)/(wl2-wl1)
      enddo
-!-- calculating wavelegth unformly
      r1 = rand()
      prt_tlyrand = prt_tlyrand+1
      wl0 = 1d0/((1d0-r1)/gas_wl(iig)+r1/gas_wl(iig+1))
-!-- calculating radial position
-     r1 = rand()
-     prt_tlyrand = prt_tlyrand+1
-     
-     prt_particles(ipart)%rsrc = (r3*gas_xarr(i+1)**3 + &
-          (1.0-r3)*gas_xarr(i)**3)**(1.0/3.0)
-     r0 = prt_particles(ipart)%rsrc
+
 !-- calculating direction cosine (comoving)
      r1 = rand()
      prt_tlyrand = prt_tlyrand+1
      mu0 = 1d0-2d0*r1
-     mu0 = max(mu0,1d-7)
-!-- calculating particle time
-     prt_particles(ipart)%tsrc = tsp_t
+
 !-- calculating particle energy
-     ep0 = gas_evolinit(i,1,1)/real(gas_nvolinit(i,1,1))
-!             gas_eext=gas_eext+ep0
-     if(gas_isvelocity) then
-        prt_particles(ipart)%Esrc = ep0*(1.0+r0*mu0/pc_c)
-        prt_particles(ipart)%Ebirth = ep0*(1.0+r0*mu0/pc_c)
-!-- velocity effects accounting
-!                 gas_evelo=gas_evelo-ep0*r0*mu0/pc_c
+     ep0 = gas_evolinit(i,j,k)/real(gas_nvolinit(i,j,k))
+
 !
-        prt_particles(ipart)%wlsrc = wl0/(1.0+r0*mu0/pc_c)
-        !
-        prt_particles(ipart)%musrc = (mu0+r0/pc_c)/&
-             (1.0+r0*mu0/pc_c)
+!-- selecting geometry
+     select case(in_igeom)
+
+!-- 1D
+     case(1)
+!-- calculating position
+        r1 = rand()
+        prt_tlyrand = prt_tlyrand+1
+        prt_particles(ipart)%rsrc = (r1*gas_xarr(i+1)**3 + &
+             (1d0-r1)*gas_xarr(i)**3)**(1d0/3d0)
+
+!-- if velocity-dependent, transforming direction
+        if(gas_isvelocity) then
+           x0 = prt_particles(ipart)%rsrc
+!-- 1+dir*v/c
+           cmffact = 1d0+x0*mu0/pc_c
+!-- mu
+           prt_particles(ipart)%musrc = (mu0+x0/pc_c)/cmffact
+        else
+           prt_particles(ipart)%musrc = mu0
+        endif
+
+!-- 2D
+     case(2)
+!-- calculating position
+        r1 = rand()
+        prt_particles(ipart)%rsrc = (r1*gas_xarr(i+1)**2 + &
+             (1d0-r1)*gas_xarr(i)**2)**(0.5d0)
+        r1 = rand()
+        prt_particles(ipart)%y = r1*gas_yarr(j+1) + &
+             (1d0-r1)*gas_yarr(j)
+
+!-- sampling azimuthal angle of direction (off radial axis)
+        r1 = rand()
+        om0 = pc_pi2*r1
+
+!-- if velocity-dependent, transforming direction
+        if(gas_isvelocity) then
+           x0 = prt_particles(ipart)%rsrc
+           y0 = prt_particles(ipart)%y
+!-- 1+dir*v/c
+           cmffact = 1d0+(mu0*y0+sqrt(1d0-mu0**2)*cos(om0)*x0)/pc_c
+           azitrfm = atan2(sqrt(1d0-mu0**2)*sin(om0), &
+                sqrt(1d0-mu0**2)*cos(om0)+x0/pc_c)
+!-- mu
+           prt_particles(ipart)%musrc = (mu0+y0/pc_c)/cmffact
+!-- om
+           if(azitrfm >= 0d0) then
+              prt_particles(ipart)%om = azitrfm
+           else
+              prt_particles(ipart)%om = azitrfm+pc_pi2
+           endif
+        else
+           prt_particles(ipart)%musrc = mu0
+           prt_particles(ipart)%om = om0
+        endif
+
+!-- 3D
+     case(3)
+        stop 'initial_particles: no 3D transport'
+     endselect
+
+!-- if velocity-dependent, transforming energy, wavelength
+     if(gas_isvelocity) then
+        prt_particles(ipart)%esrc = ep0*cmffact
+        prt_particles(ipart)%ebirth = ep0*cmffact
+        prt_particles(ipart)%wlsrc = wl0/cmffact
      else
-        prt_particles(ipart)%Esrc = ep0
-        prt_particles(ipart)%Ebirth = ep0
-
+        prt_particles(ipart)%esrc = ep0
+        prt_particles(ipart)%ebirth = ep0
         prt_particles(ipart)%wlsrc = wl0
-        !
-        prt_particles(ipart)%musrc = mu0
      endif
-     prt_particles(ipart)%rtsrc = 1
-
-!-- Setting i = zone of particle
-     prt_particles(ipart)%zsrc = i
-!-- Setting particle index to not vacant
-     prt_isvacant(ipart) = .false.
 
   enddo !ipart
 
