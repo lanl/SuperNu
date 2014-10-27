5826b8adc344cdfb2895cf3e6201f156517bac18
Ryan Wollaeger <wollaeger@wisc.edu>
2014-10-27 14:58:37 -0500

    incorporated leakage_opacity2. debugged case selection in gasgrid_update.

--- GASGRID/gasgrid_update.f
+++ GASGRID/gasgrid_update.f
@@ -221,7 +221,7 @@ c-- Calculating Fleck factor, leakage opacities
 c-- Calculating emission probabilities for each group in each cell
       call emission_probability
 c-- Calculating IMC-DDMC albedo coefficients and DDMC leakage opacities
-      select case
+      select case(in_igeom)
 c-- 1D
       case(1)
          call leakage_opacity1
