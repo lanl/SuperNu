59533b15c40c73fbe47fa7c7c6ed998bfe7783bc
Ryan Wollaeger <wollaeger@wisc.edu>
2014-11-07 20:42:22 -0600

    finished boundary crossing conditions.

--- TRANSPORT3/transport3.f90
+++ TRANSPORT3/transport3.f90
@@ -135,7 +135,7 @@ subroutine transport3(ptcl,isvacant)
 !-- finding minimum distance
   d = min(dcen,dbx,dby,dbz,dthm,dcol,ddop)
   if(any((/dcen,dbx,dby,dyz,dthm,dcol,ddop/)<0d0)) then
-     write(*,*) dcen,dbz,dbr,dthm,dcol,ddop
+     write(*,*) dcen,dbx,dby,dbz,dthm,dcol,ddop
      stop 'transport3: negative distance'
   endif
 
@@ -311,65 +311,188 @@ subroutine transport3(ptcl,isvacant)
           min(dx(ix+ihelp),dy(iy),dz(iz))*thelp < prt_tauddmc &
           .or.in_puretran) then
 !-- IMC in adjacent cell
-           ix = ix+ihelp
-        else
+        ix = ix+ihelp
+     else
 !-- DDMC in adjacent cell
-           if(gas_isvelocity) then
+        if(gas_isvelocity) then
 !-- transforming x-cosine to cmf
-              mu = (mu-x*cinv)/elabfact
-              if(mu>1d0) then
-                 mu = 1
-              elseif(mu<-1d0) then
-                 mu = -1
-              endif
-           endif
-           help = (gas_cap(ig,ix+ihelp,iy,iz)+gas_sig(ix+ihelp,iy,iz)) * &
-                dx(ix+ihelp)*thelp
-           help = 4d0/(3d0*help+6d0*pc_dext)
-!-- sampling
-           r1 = rand()
-           if (r1 < help*(1d0+1.5d0*abs(mu))) then
-              ptcl%rtsrc = 2
-              gas_methodswap(ix,iy,iz)=gas_methodswap(ix,iy,iz)+1
-              if(gas_isvelocity) then
-                 ep = ep*elabfact
-                 ep0 = ep0*elabfact
-                 wl = wl/elabfact
-              endif
-              ix = ix + ihelp
-           else
-              r1 = rand()
-              r2 = rand()
-              mu = -ihelp*max(r1,r2)
-              r1 = rand()
-              eta = sqrt(1d0-mu**2)*cos(pc_pi2*r1)
-!-- resampling z-cosine
-              xi = sqrt(1d0-mu**2)*sin(pc_pi2*r1)
-!-- resampling azimuthal
-              om = atan2(eta,mu)
-              if(om<0d0) om=om+pc_pi2
-              if(gas_isvelocity) then
-!-- transforming xi to lab
-                 xi=(xi+z*cinv)/(1d0+(x*mu+y*eta+z*xi)*cinv)
-                 if(xi>1d0) then
-                    xi = 1d0
-                 elseif(xi<-1d0) then
-                    xi = -1d0
-                 endif
-!-- transforming om to lab
-                 om = atan2(eta+y*cinv,mu+x*cinv)
-                 if(om<0d0) om=om+pc_pi2
-              endif
+           mu = (mu-x*cinv)/elabfact
+           if(mu>1d0) then
+              mu = 1
+           elseif(mu<-1d0) then
+              mu = -1
            endif
         endif
+        help = (gas_cap(ig,ix+ihelp,iy,iz)+gas_sig(ix+ihelp,iy,iz)) * &
+             dx(ix+ihelp)*thelp
+        help = 4d0/(3d0*help+6d0*pc_dext)
+!-- sampling
+        r1 = rand()
+        if (r1 < help*(1d0+1.5d0*abs(mu))) then
+           ptcl%rtsrc = 2
+           gas_methodswap(ix,iy,iz)=gas_methodswap(ix,iy,iz)+1
+           if(gas_isvelocity) then
+              ep = ep*elabfact
+              ep0 = ep0*elabfact
+              wl = wl/elabfact
+           endif
+           ix = ix + ihelp
+        else
+           r1 = rand()
+           r2 = rand()
+           mu = -ihelp*max(r1,r2)
+           r1 = rand()
+           eta = sqrt(1d0-mu**2)*cos(pc_pi2*r1)
+!-- resampling z-cosine
+           xi = sqrt(1d0-mu**2)*sin(pc_pi2*r1)
+!-- resampling azimuthal
+           om = atan2(eta,mu)
+           if(om<0d0) om=om+pc_pi2
+           if(gas_isvelocity) then
+!-- transforming xi to lab
+              xi=(xi+z*cinv)/(1d0+(x*mu+y*eta+z*xi)*cinv)
+              if(xi>1d0) then
+                 xi = 1d0
+              elseif(xi<-1d0) then
+                 xi = -1d0
+              endif
+!-- transforming om to lab
+              om = atan2(eta+y*cinv,mu+x*cinv)
+              if(om<0d0) om=om+pc_pi2
+           endif
+        endif
+     endif
 
 !
 !-- y-bound
   elseif(d==dby) then
 
+     if(eta>=0d0) then
+        ihelp = 1
+     else
+        ihelp = -1
+     endif
+
+     if((gas_cap(ig,ix,iy+ihelp,iz)+gas_sig(ix,iy+ihelp,iz)) * &
+          min(dx(ix),dy(iy+ihelp),dz(iz))*thelp < prt_tauddmc &
+          .or.in_puretran) then
+!-- IMC in adjacent cell
+        iy = iy+ihelp
+     else
+!-- DDMC in adjacent cell
+        if(gas_isvelocity) then
+!-- transforming x-cosine to cmf
+           eta = (eta-y*cinv)/elabfact
+           if(eta>1d0) then
+              eta = 1
+           elseif(eta<-1d0) then
+              eta = -1
+           endif
+        endif
+        help = (gas_cap(ig,ix,iy+ihelp,iz)+gas_sig(ix,iy+ihelp,iz)) * &
+             dy(iy+ihelp)*thelp
+        help = 4d0/(3d0*help+6d0*pc_dext)
+!-- sampling
+        r1 = rand()
+        if (r1 < help*(1d0+1.5d0*abs(eta))) then
+           ptcl%rtsrc = 2
+           gas_methodswap(ix,iy,iz)=gas_methodswap(ix,iy,iz)+1
+           if(gas_isvelocity) then
+              ep = ep*elabfact
+              ep0 = ep0*elabfact
+              wl = wl/elabfact
+           endif
+           iy = iy + ihelp
+        else
+           r1 = rand()
+           r2 = rand()
+           eta = -ihelp*max(r1,r2)
+           r1 = rand()
+           mu = sqrt(1d0-eta**2)*cos(pc_pi2*r1)
+!-- resampling z-cosine
+           xi = sqrt(1d0-eta**2)*sin(pc_pi2*r1)
+!-- resampling azimuthal
+           om = atan2(eta,mu)
+           if(om<0d0) om=om+pc_pi2
+           if(gas_isvelocity) then
+!-- transforming xi to lab
+              xi=(xi+z*cinv)/(1d0+(x*mu+y*eta+z*xi)*cinv)
+              if(xi>1d0) then
+                 xi = 1d0
+              elseif(xi<-1d0) then
+                 xi = -1d0
+              endif
+!-- transforming om to lab
+              om = atan2(eta+y*cinv,mu+x*cinv)
+              if(om<0d0) om=om+pc_pi2
+           endif
+        endif
+     endif
+
 !
 !-- z-bound
   elseif(d==dbz) then
+
+     if(xi>=0d0) then
+        ihelp = 1
+     else
+        ihelp = -1
+     endif
+
+     if((gas_cap(ig,ix,iy,iz+ihelp)+gas_sig(ix,iy,iz+ihelp)) * &
+          min(dx(ix),dy(iy),dz(iz+ihelp))*thelp < prt_tauddmc &
+          .or.in_puretran) then
+!-- IMC in adjacent cell
+        iz = iz+ihelp
+     else
+!-- DDMC in adjacent cell
+        if(gas_isvelocity) then
+!-- transforming x-cosine to cmf
+           xi = (xi-z*cinv)/elabfact
+           if(xi>1d0) then
+              xi = 1
+           elseif(xi<-1d0) then
+              xi = -1
+           endif
+        endif
+        help = (gas_cap(ig,ix,iy,iz+ihelp)+gas_sig(ix,iy,iz+ihelp)) * &
+             dz(iz+ihelp)*thelp
+        help = 4d0/(3d0*help+6d0*pc_dext)
+        !-- sampling
+        r1 = rand()
+        if (r1 < help*(1d0+1.5d0*abs(xi))) then
+           ptcl%rtsrc = 2
+           gas_methodswap(ix,iy,iz)=gas_methodswap(ix,iy,iz)+1
+           if(gas_isvelocity) then
+              ep = ep*elabfact
+              ep0 = ep0*elabfact
+              wl = wl/elabfact
+           endif
+           iz = iz + ihelp
+        else
+           r1 = rand()
+           r2 = rand()
+!-- resampling z-cosine
+           xi = -ihelp*max(r1,r2)
+           r1 = rand()
+!-- resampling azimuthal
+           om = pc_pi2*r1
+           mu = sqrt(1d0-xi**2)*cos(om)
+           eta = sqrt(1d0-xi**2)*sin(om)
+           if(gas_isvelocity) then
+!-- transforming xi to lab
+              xi=(xi+z*cinv)/(1d0+(x*mu+y*eta+z*xi)*cinv)
+              if(xi>1d0) then
+                 xi = 1d0
+              elseif(xi<-1d0) then
+                 xi = -1d0
+              endif
+!-- transforming om to lab
+              om = atan2(eta+y*cinv,mu+x*cinv)
+              if(om<0d0) om=om+pc_pi2
+           endif
+        endif
+     endif
 
 !
 !-- Doppler shift
