537d9c273ee8ffb0a68e12219349b6e7c469792c
D.R. van Rossum <daan@flash.uchicago.edu>
2014-11-21 15:09:10 -0600

    issue stop statement at particle number overflow

--- vacancies.f90
+++ vacancies.f90
@@ -10,18 +10,17 @@ subroutine vacancies
 
   integer :: ipart, ivac
 
-  !Initializing index counters and full array checking boolean
+!-- Initializing index counters and full array checking boolean
   ipart = 0
   ivac = 0
 
-  !Filling prt_vacantarr with particle index of vacant particles: loop
+!-- Filling prt_vacantarr with particle index of vacant particles: loop
   do ipart=1,prt_npartmax
+     if(.not.prt_isvacant(ipart)) cycle
+     ivac = ivac+1
+     prt_vacantarr(ivac) = ipart
      if (ivac == prt_nnew) exit
-     if (prt_isvacant(ipart)) then
-        ivac = ivac+1
-        prt_vacantarr(ivac) = ipart
-     endif
   enddo
-  if(ipart > prt_npartmax) write(6,*) 'Maximum number of prt_particles reached'
+  if(ipart > prt_npartmax) stop 'Maximum number of prt_particles reached'
 
 end subroutine vacancies
