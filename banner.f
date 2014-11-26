a35dfdc3774df3149212560982baa2d3bc00d5e0
D.R. van Rossum <daan@flash.uchicago.edu>
2014-11-26 13:55:04 -0600

    added dependencies to conditional version.inc rule

--- banner.f
+++ banner.f
@@ -12,11 +12,10 @@ c
       character*(MPI_MAX_PROCESSOR_NAME) :: pname
       integer :: ilen,ierr
 c
-      character*8  :: coderev_nr   !value set in version.inc
-      character*40 :: coderev_id   !value set in version.inc
-      character*24 :: coderev_date !value set in version.inc
-      character*28 :: build_date   !value set in version.inc
-c     character*30 :: compile_date !value set in version.inc
+      character*8  :: coderev_nr=''   !value set in version.inc
+      character*40 :: coderev_id=''   !value set in version.inc
+      character*24 :: coderev_date='' !value set in version.inc
+      character*28 :: build_date=''   !value set in version.inc
       include 'version.inc'
 c
       call date_and_time(t_startdate,t_starttime)
@@ -42,15 +41,14 @@ c
       write(6,*) "(2013/mar/05)"
       write(6,*)
 c
-      write(6,*) "code revision nr: ", coderev_nr
-      write(6,*) "code revision id: ", coderev_id
-      write(6,*) "code revis. date: ", coderev_date
-      write(6,*) "build date:       ", build_date
+      write(6,*) "code revision nr: ", trim(coderev_nr)
+      write(6,*) "code revision id: ", trim(coderev_id)
+      write(6,*) "code revis. date: ", trim(coderev_date)
+      write(6,*) "build date:       ", trim(build_date)
 c
-c     write(6,*) "build date      : ", build_date
-c     write(6,*) "build hostname  : ", build_hostname
-c     write(6,*) "compiler        : ", compiler_version
-c     write(6,*) "compiler flags  : ", compiler_flags
+c     write(6,*) "build hostname  : ", trim(build_hostname)
+c     write(6,*) "compiler        : ", trim(compiler_version)
+c     write(6,*) "compiler flags  : ", trim(compiler_flags)
 c
       write(6,*) 'simulation date : ', t_startdate//" / "//t_starttime
       write(6,*) 'processor name  : ', trim(pname)
