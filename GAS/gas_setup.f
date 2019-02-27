c97acd96219fc1e810dd03d415957a2dec17ed55
wollaege <wollaeger@lanl.gov>
2019-02-26 18:17:59 -0700

    Update copyright assertion and date ranges for supernu, v3.x.

--- GAS/gas_setup.f
+++ GAS/gas_setup.f
@@ -1,5 +1,5 @@
 *This file is part of SuperNu.  SuperNu is released under the terms of the GNU GPLv3, see COPYING.
-*Copyright (c) 2013-2017 Ryan T. Wollaeger and Daniel R. van Rossum.  All rights reserved.
+*Copyright (c) 2013-2019 Ryan T. Wollaeger and Daniel R. van Rossum.  All rights reserved.
       subroutine gas_setup
 c     --------------------
       use inputstrmod
